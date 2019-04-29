1a9f617ccb43205f0cbad5d1b875879f901dcc59
Maxime <maxime.clement94@icloud.com>
2019-04-29 13:01:18 +0200

    ajout d'un cube avec une couleur

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
 echo 'Je mets ce que je veux';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="asset/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="cube">coucou</div>
+</body>
+</html>
